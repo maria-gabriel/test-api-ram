2827883e6fddbbb38cc08700361f69a59fed37aa
maria-gabriel <85816951+maria-gabriel@users.noreply.github.com>
2023-06-19 17:50:27 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import logo from './img/rym.png';
+import './App.css';
+import { useState } from 'react';
+import Characters from './components/Characters';
+
+function App() {
+  const [characters, setCharacters] = useState(null);
+  const reqApi = async () => {
+    const api = await fetch("https://rickandmortyapi.com/api/character");
+    const charApi = await api.json();
+    setCharacters(charApi.results);
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className='title'>Rick & Morty</h1>
+        {characters ? (
+        <Characters characters ={characters} setCharacters={setCharacters}/>
+        ) : (
+        <>
+        <img src={logo} alt="rick & morty" className='img-home'/>
+        <button onClick={reqApi} className='btn-search'>Buscar personajes</button>
+        </>
+        )}
+      </header>
+    </div>
+  );
+}
+
+export default App;
